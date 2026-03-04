a8bebfe30fb82ad5d0ed0c55b0fdf522462677c7
navnishan <navnishann@gmaol.com>
2026-03-04 15:35:54 +0200

    Add API routing

---
+++ airport/urls.py
@@ -0,0 +1,25 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from airport.views import (
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    AirportViewSet,
+    CrewViewSet,
+    FlightViewSet,
+    OrderViewSet,
+    RouteViewSet,
+)
+
+router = DefaultRouter()
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("airplane-types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("crew", CrewViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet, basename="orders")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
