c0f5f5a498a43038f411cbc5a33c037299b046a3
navnishan <navnishann@gmaol.com>
2026-03-05 16:10:19 +0200

    update security properties

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^pj1^bdc&v%e#%j6f2t^nf+rl+merv3lqvxcv(49x*ln(vhnla'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,7 +90,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa501
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
